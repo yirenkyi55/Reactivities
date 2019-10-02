f3c8e6ca2c7ceeb36819c7a5997cc8bf35ee2045
Emmanuel Yirenkyi <fb_emmanuel@outlook.com>
2019-10-02 16:57:54 +0000

    fixing autocomplete with client form

--- client-app/src/app/common/form/TextAreaInput.tsx
+++ client-app/src/app/common/form/TextAreaInput.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FieldRenderProps } from "react-final-form";
-import { FormFieldProps, Form, Label } from "semantic-ui-react";
+import React from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import { FormFieldProps, Form, Label } from 'semantic-ui-react';
 
 interface IProps
   extends FieldRenderProps<string, HTMLTextAreaElement>,
@@ -15,9 +15,14 @@ const TextAreaInput: React.FC<IProps> = ({
 }) => {
   return (
     <Form.Field error={touched && !!error} width={width}>
-      <textarea {...input} placeholder={placeholder} rows={rows} />
+      <textarea
+        {...input}
+        placeholder={placeholder}
+        rows={rows}
+        autoComplete="off"
+      />
       {touched && error && (
-        <Label basic color='red'>
+        <Label basic color="red">
           {error}
         </Label>
       )}
--- client-app/src/app/common/form/TextInput.tsx
+++ client-app/src/app/common/form/TextInput.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FieldRenderProps } from "react-final-form";
-import { FormFieldProps, Form, Label } from "semantic-ui-react";
+import React from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import { FormFieldProps, Form, Label } from 'semantic-ui-react';
 
 interface IProps
   extends FieldRenderProps<string, HTMLInputElement>,
@@ -15,9 +15,9 @@ const TextInput: React.FC<IProps> = ({
 }) => {
   return (
     <Form.Field error={touched && !!error} type={type} width={width}>
-      <input {...input} placeholder={placeholder} />
+      <input {...input} placeholder={placeholder} autoComplete="off" />
       {touched && error && (
-        <Label basic color='red'>
+        <Label basic color="red">
           {error}
         </Label>
       )}
